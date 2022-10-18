faa0f87f563f71a513c6d36a10eebfb520feb319
Adeliukas <adeliuuukas@gmail.com>
2022-10-18 12:57:45 +0100

    fixed problem with units

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-alert("Hello Adele");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "ea600dd4b1532851054fb72f18bd3426";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
